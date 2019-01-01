e940741eeec6ed0c14b08f980b61e663750445db
aaron saunders <aaronksaunders@gmail.com>
2019-01-01 18:53:01 -0500

    firebase stuff

--- src/main.js
+++ src/main.js
@@ -8,12 +8,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyD3dxdxD3Wux-XUj_aaFF5tmVeyQtnelM4",
-  authDomain: "vue-app-7f475.firebaseapp.com",
-  databaseURL: "https://vue-app-7f475.firebaseio.com",
-  projectId: "vue-app-7f475",
-  storageBucket: "vue-app-7f475.appspot.com",
-  messagingSenderId: "326758384574"
+
 });
 
 Vue.config.productionTip = false;
